6736eca20affca90861d0cb4f753d8739d98b9f6
RedHeads_Enjoyer <dsc.cxc@mail.ru>
2024-06-13 18:38:28 +0300

    Сделан экран пассажира

--- client/src/pages/PassengerPage/index.jsx
+++ client/src/pages/PassengerPage/index.jsx
@@ -6,6 +6,8 @@ import {requests} from "../../datasets/requests";
 import Clock from "../../components/Clock";
 import UserBlock from "../../components/UserBlock";
 import RequestsList from "../../components/RequestsList";
+import Button from "../../components/Button";
+import InputText from "../../components/InputText";
 
 function MainPage() {
     const {id} = useParams()
@@ -16,12 +18,29 @@ function MainPage() {
         requests: [],
     })
 
+    const [changeMode, setChangeMode] = useState(false)
+
+    const [changedData, setChangedData] = useState({
+        name: '',
+        phoneNumber: ''
+    })
+
     const getPassenger = () => {
         setPassenger({
             name: 'Стрижевский Филипп Александрович',
             phoneNumber: '+7(916)494-12-21',
             requests: requests
-        });
+        })
+
+        setChangedData({
+            name: 'Стрижевский Филипп Александрович',
+            phoneNumber: '+7(916)494-12-21'
+        })
+    }
+
+    const handleChangeButton = (e) => {
+        e.preventDefault()
+        setChangeMode(true)
     }
 
     useEffect(() => {
@@ -32,8 +51,32 @@ function MainPage() {
         return () => clearTimeout(timer); // Очистка таймера при размонтировании компонента
     }, []);
 
-    const handleFiltersChange = ({currentTarget: input}) => {
+    const handlePassengerChange = ({currentTarget: input}) => {
         setPassenger({...passenger, [input.name]: input.value})
+    }
+
+    const handleChangedDataChange = ({currentTarget: input}) => {
+        setChangedData({...passenger, [input.name]: input.value})
+    }
+
+    const handleChangeSubmit = (e) => {
+        e.preventDefault()
+        setPassenger(
+            {
+                ...passenger,
+                name: changedData.name,
+                phoneNumber: changedData.phoneNumber
+            })
+        setChangeMode(false)
+    }
+
+    const handleChangeCancel = (e) => {
+        e.preventDefault()
+        setChangedData({
+            name: passenger.name,
+            phoneNumber: passenger.phoneNumber
+        })
+        setChangeMode(false)
     }
 
     return (
@@ -48,10 +91,43 @@ function MainPage() {
             <p className={classes.page__label}>{`Информация о папасажире №${id}`}</p>
             <p className={'bold__text'}>Общая информация</p>
             <div className={classes.base__info__wrapper}>
-                <p>Имя:</p>
-                <p>{passenger.name}</p>
-                <p>Номер телефона:</p>
-                <p>{passenger.phoneNumber}</p>
+                {
+                    changeMode === false ?
+                        <>
+                            <p>Имя:</p>
+                            <p>{passenger.name}</p>
+                            <p>Номер телефона:</p>
+                            <p>{passenger.phoneNumber}</p>
+                            <Button onClick={handleChangeButton}>Изменить</Button>
+                        </>
+                        :
+                        <div className={classes.change__wrapper}>
+                            <div className={classes.input__name__wrapper}>
+                                <InputText
+                                    label={'Имя'}
+                                    placeholder={'ФИО'}
+                                    name={'name'}
+                                    value={changedData.name}
+                                    onChange={handleChangedDataChange}
+                                />
+                            </div>
+                            <div className={classes.input__phone__wrapper}>
+                                <InputText
+                                    label={'Номер Телефона'}
+                                    placeholder={'ФИО'}
+                                    name={'phoneNumber'}
+                                    value={changedData.phoneNumber}
+                                    onChange={handleChangedDataChange}
+                                />
+                            </div>
+                            <div className={classes.button__wrapper}>
+                                <Button onClick={handleChangeSubmit}>Сохранить</Button>
+                            </div>
+                            <div className={classes.button__wrapper}>
+                                <Button onClick={handleChangeCancel}>Отмена</Button>
+                            </div>
+                        </div>
+                }
             </div>
             <p className={'bold__text'}>История заявок</p>
             <div className={classes.list__wrapper}>
